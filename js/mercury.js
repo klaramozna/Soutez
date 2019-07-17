0550af3f39e5dbdf773c85017a0bc99b557d9867
Nevim0 <52952440+Nevim0@users.noreply.github.com>
2019-07-17 15:01:45 +0200

    Changed styles

--- js/mercury.js
+++
@@ -1,4 +0,0 @@
-var imageMercury = document.getElementsByClassName('image-box')[0];
-
-imageMercury.style.position = "sticky";
-imageMercury.style.top = document.getElementsByClassName('header')[0].style.height + document.getElementsByClassName('content')[0].style.marginTop;
